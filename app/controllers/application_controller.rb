fc859f17f8e975a86b7fe1f3af930fc41cfead2e
Ryodai-15 <gon.4629.1208@gmail.com>
2021-08-09 07:45:16 +0000

    [Fix]conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  def after_sign_in_path_for(resource)
+    case resource
+    when Admin
+      admin_root_path
+    when Member
+      root_path
+    end
+  end
 
   protected
 
