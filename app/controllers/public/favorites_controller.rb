36ca6422d1473f6d899b1814f4999c5ed7ad0cad
Ryodai-15 <gon.4629.1208@gmail.com>
2021-08-20 07:59:20 +0000

    [finish]fava-rank

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,23 @@
 class Public::FavoritesController < ApplicationController
-  
+
   def create
-    recipe = Recipe.find(params[:recipe_id])
-    favorite = current_member.favorites.new(recipe_id: recipe.id)
+    # recipe = Recipe.find(params[:recipe_id])
+    # favorite = current_member.favorites.new(recipe_id: recipe.id)
+    # 上が以前のコントローラ
+    @recipe = Recipe.find(params[:recipe_id])
+    favorite = @recipe.favorites.new(member_id: current_member.id)
     favorite.save
-    redirect_to product_recipe_path(recipe.product_id,recipe.id)
+    # redirect_to product_recipe_path(recipe.product_id,recipe.id)→jsリクエストのため
   end
-  
+
   def destroy
-    recipe = Recipe.find(params[:recipe_id])
-    favorite = current_member.favorites.find_by(recipe_id: recipe.id)
+    # recipe = Recipe.find(params[:recipe_id])
+    # favorite = current_member.favorites.find_by(recipe_id: recipe.id)
+    # 上が以前のコントローラ
+    @recipe = Recipe.find(params[:recipe_id])
+    favorite = @recipe.favorites.find_by(member_id: current_member.id)
     favorite.destroy
-    redirect_to product_recipe_path(recipe.product_id,recipe.id)
+    # redirect_to product_recipe_path(recipe.product_id,recipe.id)→jsリクエストのため
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
 
   root :to => "public/homes#top"
   get "home/about" => "public/homes#about"
-  
+
   # ゲストログイン機能
   post '/homes/guest_sign_in', to: 'public/homes#guest_sign_in'
-   
+
   devise_scope :member do
     post 'members/guest_sign_in', to: 'public/members/sessions#guest_sign_in'
   end
-  
+
   # 検索機能ルーティング
   get '/search', to: 'public/searchs#search'
 
@@ -26,26 +26,33 @@ Rails.application.routes.draw do
         patch 'withdraw'
       end
     end
-    
+
     get "members/:id/favorites" => "members#favorites", as: 'member_favorites'
-    
+
     resources :products, only: [:index, :show] do
       resources :recipes do
         resource :favorites, only: [:create, :destroy]
         resources :reviews, only: [:index, :create]
       end
     end
+
+    # お問い合わせフォーム
+    resources :contacts, only: [:new, :create]
+      post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
+      post 'contacts/back', to: 'contacts#back', as: 'back'
+      get 'done', to: 'contacts#done', as: 'done'
+
   end
-  
+
   namespace :admin do
     root :to => "homes#top"
     resources :members, only: [:index, :show, :edit, :update]
     resources :products, only: [:new, :create, :index, :show, :edit, :update]
   end
-  
-  
+
+
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
   }
-  
+
 end
